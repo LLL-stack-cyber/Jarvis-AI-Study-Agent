bfd5c023da8924035290b7380537bfa90c164736
Shreyas <letslearnatleisure@gmail.com>
2026-03-14 18:44:05 +0530

    Remove unused import of mentor module

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import mentor
+
 from backend.api.auth import router as auth_router
 from backend.api.mentor import router as mentor_router
 from backend.api.quiz import router as quiz_router
